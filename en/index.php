27a014a6fb55d5ba7d937059dd6cdce633dbf2b7
Enno Rehling <enno.rehling@gmail.com>
2016-01-08 12:45:45 +0100

    New English homepage text, new approach aimed at players first, game masters and developers last.

--- en/index.php
+++ en/index.php
@@ -10,57 +10,33 @@
 <? include "includes/navigation.php"; ?>
 <? include "includes/righthand.php"; ?>
 <div id="center">
-    <h3>Eressea - Germany's most popular PBEM now speaks
-    English</h3>
-    <p>Eressea is a turn-based strategy game in a fantasy
-    setting. It is an open-ended game, adding new players to
-    the fringes of the world all the time. The game is free of
-    charge, has around 1700 players and is growing rapidly even
-    after more than six years.</p>
-    <p>The original game was written for a German speaking
-    audience, and there is plenty of German material available
-    on the web. Since early 2002 we're accepting
-    English-speaking players as well, after having done a
-    massive amount of localization.
-    </p>
-    <div align="right"><a href="about.php">&gt;&gt; Getting started</a></div>
-    <h3>Registration</h3>
-    <p>Chances are you've never played Eressea before, and need
-    to learn the basics of the game first, we've got a tutorial
-    game that lets you do just that. The registration pages let
-    you sign up for a game, and depending on whether you're a
-    returning player or a new recruit, signs you up for either
-    game.
-    </p>
-    <div align="right"><a href="register.php">&gt;&gt; To the registration
-    pages</a></div>
-    <h3>Rules and Downloads</h3>
-    <p>Compared to other games, Eressea is fairly complex, and
-    has a pretty big manual. Unlike other games, you'll really
-    have to read this manual before you start playing. The
-    rules are avaliable online, and so are a number of tools
-    that simplify playing, most notably Magellan, a graphical
-    client for the game.
-    </p>
-    <div align="right"><a href="http://wiki.eressea.de/en/">&gt;&gt; We have a wiki for rules and stuff</a></div>
-    <br />
-    <div align="right"><a href="http://sourceforge.net/projects/magellan-client/">
-    &gt;&gt; Get Magellan</a></div>
-    <h3>Questions?</h3>
-    <p>You probably have a few. There are different ways to get
-    them answered. You can reach us at 
-    <a href="mailto:info@eressea.de">info@eressea.de</a>, but
-    keep in mind that we're rather busy and providing Eressea
-    for free. We have regular jobs that don't leave us time to
-    answer even one question per month for each of the 1700
-    players - that would be more than 400 a week! Definitely
-    the best way to get a quick answer is IRC. There is a
-    channel #eressea on IRCnet (irc.stealth.net is a popular
-    American server) and the people there are a friendly and
-    helpful bunch. As an alternative, there is also a mailing
-    list of experienced players answering questions at 
-    <a href="mailto:eressea-help@eressea.kn-bremen.de">
-    eressea-help@eressea.kn-bremen.de</a>.</p>
+<h3>20 Years of Eressea</h3>
+
+<p>When the world of Eressea was created in 1996, it was a playground for a small group of friends who were looking for an environment for their role-playing game characters to live in. As such, it is a world of magic and heroes, with the classic fantasy elements of elves, dwarves, orcs, swords and sorcery. As time went on, these original players invited their friends into the game, and eventually their friends, until everyone who was able to find the game on that new-fangled world wide web was allowed to join.</p>
+
+<p>The game eventually grew to almost three thousand active players, at which point the management threw their hands up in the air and admitted defeat, closing new registrations in an attempt to let the player base decrease organically. With time, only a hard core of a few hundred players remained, the management found that they had the time and motivation to keep the game running indefinitely, and looked at ways to revitalize the game. Initially, we started running more games, some of which have since ended, and two that are still around, known as Eressea 3 and Deveron, each with slightly modified rules and a differeent mix of players. Finally, starting in 2015, the original game started accepting new players to re-populate the world in areas where old players had left, trying to make a life in the wastelands left behind by decades of warfare and economic exploitation, all the while rediscovering the ruins of lost civilizations.</p>
+
+<h3>How do you play this game?</h3>
+
+<p>Eressea is a turn-based email game, a descendant of play-by-mail games like correspondence chess. Players control a faction consisting of units, who can be anything from magicians, traders and archers to sailors, blacksmiths and swordsmen. Every week, each player receives an email with a turn report containing describing anything their units can observe about the world, and in response, players send out an email with their orders, that is instructions for what they want each unit to do for the next turn. For example, units can move around, learn new skills, produce resources and goods, or attack other units. Turns are run weekly on Saturday nights, and a program takes the order emails from each player to calculate a new game state. Finally, new reports are generated for each player and sent out, and thus the cycle begins again.</p>
+
+<p>If you've ever played correspondence chess, a week per move may sound like an eternity, but consider that you never have more than 16 tokens on the board, and only need to give an order for a single one of them. In Eressea, factions can consist of hundreds of units, each of which needs to be given instructions on what to do. Additionally, players form alliances and diplomatic relations with their neighbors, which require additional emails to be sent and responded to, so there is plenty to do in the course of that week.</p>
+
+<p>In other words, Eressea is a hobby that can take up a considerable amount of time, and by the time we reach turn number 1,000 in October of 2016, empires will have risen and fallen, and new empires built in the ruins of those first empires, and fallen again. In many ways, Eressea is not so much a game as it is a sandbox that generates stories, and a world with a rich history, in which each player plays a modest part. Most importantly, Eressea is not a game that can be "won" in any classic sense of that word.</p>
+
+<h3>How to join the game</h3>
+
+<p>At this time, joining the game is challenging, and a number of caveats apply. The English rules page has rotted over the years, and most active players are native German speakers. There are a few English speakers in the game, and aside from the lack of a manual, the game can be played in English as well as in German, but the community of non-Germans is not big enough to even run their own web forum at this time. Despite all these warnings, we'd love to have you, and apologize that it's such a challenge. A lot of Eressea's infrastructure has been built by it's community, so if you join us, maybe you can help improve the situation for those who come later? We'd certainly appreciate it! Similarly, if there is a resource that's unavailable in your language, and Google Translate is not doing the job well enough, just ask! We're nice people, and happy to help.</p>
+<p>
+With all those warnings out of the way, if you want to start life in a semi-comfortable area of the world with not too many enemies around you, then you should put your email address on our <a href="http://www.eressea.de/de/register.php">waiting list for new signups</a>, and we'll usually get back to you in a matter of some weeks.
+
+<h3>Running your own game</h3>
+
+<p>If you are feeling especially ambitious, and want to start your own game with friends just like we did 20 years ago, that's also an option! Eressea's game server is an open source project, which means that anyone can grab the source from our <a href="https://github.com/eressea/server">github repository</a> and compile it themselves. This is not for the faint of heart, and some experience with the C programming language might be required. We're always interested in spreading the love, though, so if you get stuck, please send an email and we'll help you get going. You'll be glad to hear that unlike the game rules, most of the source code and it's documentation is in English.</p>
+
+<h3>Modifying the game</h3>
+
+<p>The code of Eressea was built with the idea in mind that different worlds would have different rules and game elements. The three active games make use of this modular design, and each new game introduced some new races, resources, items or experimental new rules. Configuration of the game is done through a mixture of configuration files and Lua scripts, and the occasional addition of more C code to the core game. If you want to become part of the development of the game, or just modify the code to build your own variant, you should join our <a href="https://eressea.slack.com">slack channel</a> to discuss your plans.</p>
 </div>
 <? include "includes/footer.php"; ?>
 </div>
