1f932169d82e2341d1143f0b397b6cc81fc23cd8
Enno Rehling <enno.rehling@gmail.com>
2016-01-17 23:14:15 +0100

    meta is not a self-closing tag

--- source/de/includes/header.php
+++ source/de/includes/header.php
@@ -1,6 +1,6 @@
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
-<meta name="Keywords" content="Eressea,PBeM,Postspiel,fantasy,strategie,rollenspiel" />
-<meta name="author" content="Enno Rehling - webmaster@eressea.de" />
-<meta name="abstract" content="Eressea" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></meta> 
+<meta name="Keywords" content="Eressea,PBeM,Postspiel,fantasy,strategie,rollenspiel" ></meta>
+<meta name="author" content="Enno Rehling - webmaster@eressea.de" ></meta>
+<meta name="abstract" content="Eressea" ></meta>
 <link rel="stylesheet" type="text/css" href="../default.css" title="default"/>
 <? include "../includes/icra.php"; ?>
