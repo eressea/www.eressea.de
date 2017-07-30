5d010324e93ce9f99a920c9bf112f66de630ec86
Enno Rehling <enno.rehling@gmail.com>
2017-07-30 11:03:01 +0200

    wir nehmen wieder Spieler auf

--- source/de/index.php
+++ source/de/index.php
@@ -5,25 +5,15 @@
 <?php include "../includes/dragon.php"; ?>
 <?php include "includes/header.php"; ?>
 <title>Eressea | Home</title>
-<script type="text/javascript">
-	/* <![CDATA[ */
-	    (function() {
-	        var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
-	        s.type = 'text/javascript';
-	        s.async = true;
-	        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
-	        t.parentNode.insertBefore(s, t);
-	    })();
-	/* ]]> */</script>
 </head>
 <body><div id="container">
 <?php include "includes/banner.php"; ?>
 <?php include "includes/navigation.php"; ?>
 <?php include "includes/righthand.php"; ?>
 <div id="center">
-  <h3>20 Jahre Eressea</h3>
+<h3>20 Jahre Eressea</h3>
 
-<p>Vor nun beinahe 20 Jahren begann das Leben auf der Welt von Eressea.
+<p>Vor mehr als 20 Jahren begann das Leben auf der Welt von Eressea.
 Eine kleine Gruppe von Spielern um Katja Zedel und Christian
 Schlittchen beschloss, die Simulation einer Fantasy-Welt ihrem
 Computer zu &uuml;berlassen, so dass jeder von ihnen die Geschicke eines
@@ -37,46 +27,55 @@ Anzahl der in ihr lebenden V&ouml;lker. Kriege wurden gefochten, unbekannte
 Teile der Welt erforscht, und die Regeln des Spieles kontinuierlich
 verbessert.</p>
 
-  <div align="right"><a href="about.php">&gt;&gt; Mehr &uuml;ber Eressea</a></div>
+<p>Anfangs haben wir jede Woche neue Spieler aufgenommen, und in das Spiel
+integriert. Mit der Zeit wurde das immer schwieriger, und wir haben
+statt dessen zwei weitere Welten (E3 und Deveron) mit leicht 
+abgewandelten Regeln gegr&uuml;ndet, in denen keine weiteren Spieler
+aufgenommen wurden.</p>
 
-  <h3>Wo ist die Anmeldung hin?</h3>
+    <div align="right"><a href="about.php">
+    &gt;&gt;&nbsp;Mehr &uuml;ber Eressea</a></div>
 
-  <p>Anfangs haben wir jede Woche neue Spieler aufgenommen, und in das Spiel
-  integriert. Mit der Zeit ist das immer schwieriger geworden, und wir haben
-  statt dessen zwei weitere Welten (E3 und Deveron) mit leicht 
-  abgewandelten Regeln gegr&uuml;ndet.</p>
+<h3>Wo kann ich mich anmelden?</h3>
 
-  <p>Entsprechend viel Zeit ist vergangen, seit eine neue Partei in Eressea
-  gestartet ist, und heute ist der Abstand zwischen einer Altpartei und einem
-  neuen Spieler enorm. Trotzdem haben wir beschlossen, zumindest experimentell
-  wieder neue Spieler in das Originalspiel aufzunehmen, weil sich dort
-  teilweise gro&szlig;e L&uuml;cken in der Besiedlung aufgetan haben. Das hat momentan
-  noch den Status eines Experiments, und es ist nicht sicher, dass diese neuen
-  Parteien auf Dauer lebensf&auml;hig sind! Falls Du eine solche Partei spielen
-  m&ouml;chtest, trage Dich auf der Warteliste f&uuml;r neue Anmeldungen ein.</p>
-  <div align="right"><a href="register.php">&gt;&gt; Zur Warteliste</a></div>
+<p>Nach einer langen Pause nehmen wir jetzt wieder neue Spieler in 
+die Originalwelt von Eressea auf, auch wenn dort inzwischen 20 Jahre
+vergangen sind, und einige der fr&uuml;hen Spieler enorme Reiche 
+gegr&uuml;ndet haben.</p>
 
-  <h3>Regeln und Downloads</h3>
-  <p>Verglichen mit anderen Spielen ist Eressea sehr komplex, und hat ein
-  relativ langes Regelbuch, das Du vor dem Spielen unbedingt gelesen haben
-  solltest. Du kannst es entweder als PDF-Datei oder online als HTML
-  lesen. Au&szlig;er den Regeln gibt es auch noch verschiedene Tools zum
-  Download, die das Spiel vereinfachen k&ouml;nnen.</p>
-  <div align="right"><a href="https://wiki.eressea.de/index.php/Hilfsmittel">&gt;&gt; Zu den Downloads</a></div>
+<p>Das passiert nicht mehr w&ouml;chentlich, sondern wann immer sich
+genug Anmedlungen gefunden haben, es kann also ein paar Wochen oder Monate dauern,
+bis Du von uns h&ouml;rst. Wenn Du mitspielen m&ouml;chtest, trage Dich bite auf
+unserer Warteliste Anmeldungen ein.</p>
+<div align="right"><a href="register.php">
+&gt;&gt;&nbsp;Zur Warteliste</a></div>
 
-  <p>
-<!--
-  <script type="text/javascript"
-        data-gratipay-username="badgerman"
-        data-gratipay-widget="button"
-        src="//grtp.co/v1.js"></script>
-   <img src="//img.shields.io/gratipay/badgerman.svg"/>
--->
-</p>   
+<h3>Regeln und Downloads</h3>
 
-   <a class="FlattrButton" style="display:none;" href="http://www.eressea.de/"></a>
-	<noscript><a href="http://flattr.com/thing/681354/Eressea" target="_blank">
-	<img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a></noscript>
+<p>Verglichen mit anderen Spielen ist Eressea sehr komplex, und hat ein
+relativ langes Regelbuch, das Du vor dem Spielen unbedingt gelesen haben
+solltest. Da sich die Regeln gelegentlich &auml;ndern, haben wir ein 
+<a href="https://wiki.eressea.de/">Wiki</a> f&uuml;r sie eingerichtet.</p>
+
+      <div align="right"><a href="https://wiki.eressea.de/">
+      &gt;&gt;&nbsp;Zu den Regeln</a></div>
+
+<p>Au&szlig;er den Regeln gibt es auch noch verschiedene Tools zum
+Download, die das Spiel vereinfachen k&ouml;nnen, auch diese sind
+im Wiki aufgelistet:</p>
+
+    <div align="right"><a href="https://wiki.eressea.de/index.php/Hilfsmittel">
+    &gt;&gt;&nbsp;Zu den Downloads</a></div>
+
+<h3>F&uuml;r Spielleiter</h3>
+
+<p>Der Quelltext von Eressea ist Open Source, und jeder, der gerne selber eine
+Partie starten m&ouml;chte, kann sich alle notwendige Software daf&uuml;r selber
+kompilieren. Der Code liegt auf github, und bei Problemen oder Fragen ist Enno
+gerne bereit zu helfen.</p>
+
+    <div align="right"><a href="https://github.com/eressea/server">
+    &gt;&gt;&nbsp;Eressea Server auf Github</a></div>
 
 </div>
 <?php include "includes/footer.php"; ?>
