99cf7bdce1459c7c98f372f9fbaae704debeaa3a
Enno Rehling <enno.rehling@gmail.com>
2020-04-16 18:18:51 +0200

    fix syntax errors, remove old upload logic

--- source/de/includes/footer.php
+++ source/de/includes/footer.php
@@ -1,4 +1,4 @@
-<div id="foot"></div>
+<div id="foot">
 <p>Eressea ist ein Spiel von Enno Rehling, Katja Zedel und Christian Schlittchen.</p>
 <p>Icons von <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> auf <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
 </p>
